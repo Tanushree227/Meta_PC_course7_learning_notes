4c56ece04b1e11fcbdab83b6de9727bf445ea0e8
Tanushree227 <tanushreedas813@gmail.com>
2024-04-10 00:27:20 +0530

    completed home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        <h1 className='text-center font-semibold'>Principles of UX/UI Design</h1>
+      <h1 className="text-center font-semibold">Principles of UX/UI Design</h1>
+      <div className="shadow-lg p-5 bg-stone-200 text-justify mt-5">
+        <p>
+          UI/UX plays a vital role for websites or any other digital product as
+          it directly impacts user interaction and engagement. UI and UX are two
+          different but interrelated concepts in the design of a web page or a
+          digital product. In simple words, UI is like a visible part of a
+          product and UX is the concept behind it.
+        </p>
+        <br />
+        <p>
+          UI/UX design is often undermined by the impact it creates on a website
+          or an application. companies and startups realize it late how crucial
+          UI/UX design is in the making of any digital product, website, or
+          application. UI/UX is often misunderstood as making a design file that
+          has the blueprint of the entire website or application. But in truth,
+          UI/UX design is way more than that. It even includes research,
+          presenting data for business strategy, wireframes, prototypes,
+          customer feedback, graphic design, animations, and a lot more.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
