14a8dbf3fe2b449942451f7d4429bccac1036450
Tanushree227 <tanushreedas813@gmail.com>
2024-04-09 23:57:57 +0530

    added custom navbar and layout changed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 import classes from './Navbar.module.css';
 
 function Navbar()
 {
     return(
-        <div></div>
+        <header className={classes.header}>
+            <div className={classes.logo}>EchoHub</div>
+            <nav>
+                <ul>
+                    <li><Link to="/" exact>Home</Link></li>
+                    <li><Link to="/week1">Week1</Link></li>
+                    <li><Link to="/week2">Week2</Link></li>
+                    <li><Link to="/week3">Week2</Link></li>
+                    <li><Link to="/week4">Week2</Link></li>
+                    <li><Link to="/week5">Week2</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
