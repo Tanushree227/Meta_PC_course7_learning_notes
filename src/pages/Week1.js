9343eaac570a41f46f176bea3392807f7615e2ff
Tanushree227 <tanushreedas813@gmail.com>
2024-04-12 22:50:26 +0530

    added definition of UX and modified tailwind classes

--- src/pages/Week1.js
+++ src/pages/Week1.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Week1() {
   return (
-    <div className='shadow-lg bg-slate-200 text-center'>
-        <h1>Week1</h1>
-        <span>What is UX?</span>
-        <p></p>
+    <div className="shadow-lg bg-slate-200 p-6">
+      <h1 className="text-center">Week1</h1>
+      <span className="font-semibold text-lg">What is UX?</span>
+      <p className="text-justify">
+        User experience (UX) design is the process design teams use to create
+        products that provide meaningful and relevant experiences to users. 
+        <br />UX design involves the design of the entire process of acquiring and
+        integrating the product, including aspects of branding, design,
+        usability and function.
+      </p>
     </div>
-  )
+  );
 }
