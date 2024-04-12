38a67127701f7039f439e8e0161fa0d75b28c4bf
Tanushree227 <tanushreedas813@gmail.com>
2024-04-12 22:58:02 +0530

    user centered design

--- src/pages/Week1.js
+++ src/pages/Week1.js
@@ -7,10 +7,32 @@ export default function Week1() {
       <span className="font-semibold text-lg">What is UX?</span>
       <p className="text-justify">
         User experience (UX) design is the process design teams use to create
-        products that provide meaningful and relevant experiences to users. 
-        <br />UX design involves the design of the entire process of acquiring and
+        products that provide meaningful and relevant experiences to users.
+        <br />
+        UX design involves the design of the entire process of acquiring and
         integrating the product, including aspects of branding, design,
         usability and function.
+      </p>
+      <span className="font-semibold text-lg">UX Design is User-Centered</span>
+      <p className="text-justify">
+        Since UX design encompasses the entire user journey, it’s a
+        multidisciplinary field–UX designers come from various backgrounds, such
+        as visual design, programming, psychology and interaction design. To
+        design for human users also means working with a heightened scope
+        regarding accessibility and accommodating many potential users’ physical
+        limitations, such as reading small text.
+      </p>
+      <br />
+      <p className="text-justify">
+        A UX designers typical tasks vary but often include user research,
+        creating personas, designing wireframes and interactive prototypes, and
+        testing designs. These tasks can vary significantly from one
+        organization to the next. Still, they always demand designers to be the
+        users advocates and keep their needs at the center of all design and
+        development efforts. Thats also why most UX designers work in some form
+        of user-centered work process and keep channeling their best-informed
+        efforts until they optimally address all of the relevant issues and user
+        needs.
       </p>
     </div>
   );
